89cde7f38df8819326e21330cb05f0c11d309cab
Mariana <marianacorrea127@hotmail.com>
2019-10-19 20:59:57 -0300

    Empezando con php

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+ <? php require head.php  ?>
+ <link rel="stylesheet" href="css/style.css">
+ <style>
+  .bg-home{
+    background-color: rgba(200,250,45.1);
+  }
+ </style>
+</head>
+<body>
+<? php require navbar.php ?>
+<div class="container bg-home">
+  <!--Carousel-->
+  <? php require carousel.php ?>
+<!--SECCION DE  ANUNCIO DE Descuentos-->
+<main>
+  <section class="row">
+    <div class="col-12 my-md-4">
+      <div class="row">
+        <figure class=" d-inline-block p-0 m-0 col-md-4 text-center">
+          <img src="imagen-mkt/jasmin-34floedit.jpg" alt="" class="img-thumbnail w-75 rounded-circle">
+        </figure>
+        <figure class=" d-inline-block p-0 m-0 col-md-4 text-center">
+          <img src="imagen-mkt/fruits-223tedit.jpg" alt="" class="img-thumbnail w-75 rounded-circle">
+        </figure>
+        <figure class=" d-inline-block p-0 m-0 col-md-4 text-center">
+          <img src="imagen-mkt/bamboo-82edit.jpg" alt="" class="img-thumbnail w-75 rounded-circle">
+        </figure>
+      </div>
+    </div>
+
+<!--FINAL SECCION DE  ANUNCIO DE Descuentos-->
+
+<!--JUMBOTRON-->
+    <div class="col-12 mt-5" id="about_us" >
+      <div class="jumbotron" id="quienessomos">
+        <h1 class="display-4">Bienvenido a Essencials!</h1>
+        <p class="lead">Nuestra finalidad es crear cosméticos naturales, con materias primas seleccionadas, de eficacia comprobada, buscando mantener y potencias sus cualidades y beneficios. <br>
+          Utilizamos aceites esenciales puros, por sus exquisitos aromas y propiedades.</p>
+        <hr class="my-4">
+        <p>No testeamos en animales.</p>
+
+      </div>
+    </div>
+  </section>
+<!--FINAL JUMBOTRON-->
+
+  <!--imagenes de producto-->
+
+  <section class="container-fluid p-0 articulos-ecommerce">
+    <div class="row px-1">
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3  ">
+        <img src="imagen-mkt/cream-foto2.jpg" class="img-fluid" alt="">
+
+        <h4>Crema aclarante</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+
+      </article>
+
+
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/face-cream.jpg" class="img-fluid" alt="">
+
+        <h4>Tónico facial</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+
+      </article>
+
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/slideshow/red-publicidad13.png" class="img-fluid" alt="">
+
+        <h4>Anti arrugas</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+
+      </article>
+
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/soap-02.jpg" class="img-fluid" alt="">
+
+        <h4>Jabón exfoliante</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+
+      </article>
+
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/soap-03.jpg" class="img-fluid" alt="">
+        <h4>Jabón de avena</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/publicidad11.jpg" class="img-fluid" alt="">
+        <h4>Crema de día</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/gel-ducha (2).jpg" class="img-fluid" alt="">
+        <h4>Crema de manos</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/soap-04.jpg" class="img-fluid" alt="">
+        <h4>Jabón de glicerina</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/cream-foto4.jpg" class="img-fluid" alt="">
+        <h4>Crema de cacao</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/crema-natural02.jpg" class="img-fluid" alt="">
+        <h4>Contorno de ojos</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/producto3.jpg" class="img-fluid" alt="">
+        <h4>Set de limpieza</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+      <article class="col-12 col-md-4 text-center my-3 bg-light pt-3 py-3 ">
+        <img src="imagen-mkt/crema-natural03.jpg" class="img-fluid" alt="">
+        <h4>Crema hidratante</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do irure dolor in repreh. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type="button" class="btn btn-success">Comprar</button>
+      </article>
+
+    </div>
+  </section>
+
+  <!--finaliza LISTADO DE PRODUCTOS-->
+
+
+</main>
+  <!--finaliza LISTADO DE PRODUCTOS-->
+
+<!--FOOTER-->
+ <? php require footer.php ?>
+ <? php require scripts.php ?>
+</body>
+
+</html>
