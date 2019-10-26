cb3b7d0296d48bd3bcae051932e80ee0dab3cbef
mario <mariones158@gmail.com>
2019-10-25 21:45:45 -0300

    Iniciamos con PHP 25/10

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'functions/indexFunctions.php';
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require 'modulos/head.php'; ?>
+  <title>Home</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+  <?php require 'modulos/navbar.php'; ?>
+</header>
+<main>
+
+</main>
+<footer class="page-footer font-small blue pt-4">
+    <?php  require 'modulos/footer.php'; ?>
+</footer>
+</body>
+</html>
---
+++ modulos/navbar.php
@@ -0,0 +1,45 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-success p-1 ">
+  <a class="navbar-brand" href="#">
+    <i class="fas fa-leaf"></i>
+    Essencials</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav ml-auto mt-2 mt-lg-4 pr-md-5">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#quienessomos">Nosotros</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="contacto.html">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="faq.html">Preguntas frecuentes</a>
+      </li>
+      <li class="nav-item dropdown">
+       <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+         Usuarios
+       </a>
+       <div class="dropdown-menu">
+         <a class="dropdown-item" href="login.html">Ingresar</a>
+         <a class="dropdown-item" href="registro.html">Registrarse</a>
+       </div>
+     </li>
+     <li class="nav-item dropdown mr-md-5">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <i class="text-light fas fa-shopping-cart"></i>
+        <i style ="color:white;" id="cart_n"></i>
+
+      </a>
+      <ul class="dropdown-menu">
+        <li class="dropdown-item" href="producto.html">CREMAS</li>
+        <li class="dropdown-item" href="producto.html">JABONES</li>
+      </ul>
+    </li>
+    </ul>
+  </div>
+</nav>
