a31a9d3d66742ea552c946426c6edeb36da6b2dc
Soledad97 <soledad.maldonado97@gmail.com>
2019-10-30 20:37:37 -0300

    Formulario de login, actualizado

--- login.php
+++ login.php
@@ -11,32 +11,37 @@ if($_POST){
    <link rel="stylesheet" href="css/style.css">
  </head>
  <body class="bg-home">
- <header>
-   <?php require ("modulos/navbar.php"); ?>
- </header>
-<main class="container-fluid p-2 m-4" align="center">
-    <h3>Ingresar</h3>
-    <form class="Ingreso" action="login.php" method="post">
-      <p>
-        <label for="email"></label>
-        <input id="email"type="email" name="email" value="" placeholder="E-mail">
-      </p>
-      <p>
-        <label for="password"></label>
-        <input id="password"type="password" name="password" value="" placeholder="Contraseña">
-      </p>
-      <div>
-        <a href="#" class="olvido">
-          <u>Olvidé mi contraseña<u>
-        </a>
+   <header>
+     <?php require ("modulos/navbar.php"); ?>
+   </header>
+  <main class="container-fluid p-2 m-4" align="center">
+    <div class="row">
+      <div class="col-12 col-md-6 mx-md-auto bg-success p-5 my-5">
+        <h3 class="text-center text-light"><u>Ingresar<u></h3>
       </div>
-      <br>
-          <p>
-        <input class="btn btn-success"type="submit" name="" value="Enviar">
-      </p>
-
-    </form>
-    </main>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-6 mx-md-auto">
+        <form class="" action="" method="post">
+          <div class="form-row">
+            <div class="col-12 col-md-6">
+              <input id="email"type="email" class="form-control" name="email" value="" placeholder="E-mail">
+            </div>
+            <div class="col-12 col-md-6">
+              <input id="password" class="form-control" type="password" name="password" value="" placeholder="Contraseña">
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group mx-auto my-5">
+              <button class="btn  btn-success" type="submit">
+                Enviar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
     <footer class="page-footer font-small blue pt-4">
         <?php  require ("modulos/footer.php"); ?>
     </footer>
